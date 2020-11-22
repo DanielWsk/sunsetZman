1528b9bc30067dfdbb4179d6cf9f4ce187ee6aac
DanielWs <danielaaronw@gmail.com>
2020-11-21 23:30:00 -0800

    Initial Commit
    All the necessary files

---
+++ app.js
@@ -0,0 +1,9 @@
+function zmanFunction() {
+    document.getElementById("candle").innerHTML = "Candlelighting: 4:26";
+    document.getElementById("sunset").innerHTML = "Sunset: 4:44";
+  }
+
+
+
+
+document.getElementById("click").onclick = function() {zmanFunction()};
